90dc12a3d4ba8e04e580f1851d1d30bfbcc3b185
Yuhui Shi <yuhuishi@umich.edu>
2020-05-25 00:04:14 -0700

    fix bi-LSTM hidden and cell state shape comments

--- 3-3.Bi-LSTM/Bi-LSTM-Torch.py
+++ 3-3.Bi-LSTM/Bi-LSTM-Torch.py
@@ -47,8 +47,8 @@ class BiLSTM(nn.Module):
     def forward(self, X):
         input = X.transpose(0, 1)  # input : [n_step, batch_size, n_class]
 
-        hidden_state = Variable(torch.zeros(1*2, len(X), n_hidden))   # [num_layers(=1) * num_directions(=1), batch_size, n_hidden]
-        cell_state = Variable(torch.zeros(1*2, len(X), n_hidden))     # [num_layers(=1) * num_directions(=1), batch_size, n_hidden]
+        hidden_state = Variable(torch.zeros(1*2, len(X), n_hidden))   # [num_layers(=1) * num_directions(=2), batch_size, n_hidden]
+        cell_state = Variable(torch.zeros(1*2, len(X), n_hidden))     # [num_layers(=1) * num_directions(=2), batch_size, n_hidden]
 
         outputs, (_, _) = self.lstm(input, (hidden_state, cell_state))
         outputs = outputs[-1]  # [batch_size, n_hidden]
